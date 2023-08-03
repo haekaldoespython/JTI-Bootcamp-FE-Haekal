a62918d6dc3eca202f915074663bd6b7ccfe747e
unknown <haekal.python@gmail.com>
2023-08-03 15:55:55 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { TodoService } from './services/todo.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoService]
+})
+
+
+export class AppComponent {
+  title = 'angular-to-do-list';
+}
+
+
